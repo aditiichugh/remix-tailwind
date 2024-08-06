68ea056cb4266ecf63a3953ddaf4b4113db02cd9
Aditi Chugh <aditichugh2606@gmail.com>
2024-08-06 23:56:11 +0530

    updated hello.jsx with bigger font size

--- app/routes/hello.jsx
+++ app/routes/hello.jsx
@@ -1,7 +1,7 @@
 export default function hello() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="bg-slate-50 text-2xl font-bold text-gray-800 p-4 rounded-lg shadow-md">
+      <h1 className="bg-slate-50 text-5xl font-bold text-gray-800 p-4 rounded-lg shadow-md">
         Hello World
       </h1>
     </div>
